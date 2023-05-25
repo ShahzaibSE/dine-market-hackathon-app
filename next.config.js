60fdb4be2243b6752a061ec3f0441208678ff014
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-05-25 20:38:46 +0500

    FEAT: Connected to vercel/postgre database

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    psql: "postgres://default:Zmb7SkWxwI0g@ep-icy-hill-346142.us-east-1.postgres.vercel-storage.com:5432/verceldb",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
