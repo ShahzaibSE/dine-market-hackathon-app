5f44b550cbe01c0e8b10249a4aac5c67d3657090
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-06-03 22:29:10 +0500

    FEAT: Creating responsive navbar & uploading assets to sanity.io

---
+++ components/home/home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function HomeComponent() {
+  return (
+    <div className='container'>
+        <div className="flex">
+            <h1>This is Home Page</h1>
+        </div>
+    </div>
+  )
+}
