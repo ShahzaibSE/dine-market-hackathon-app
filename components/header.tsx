ad7f4026341edc1a9bf0b9d3f55f0ae604ce189b
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-06-11 19:40:40 +0500

    FEAT: Designing mobile drawer for mobile screen

--- components/header.tsx
+++ components/header.tsx
@@ -22,34 +22,17 @@ export default function Header() {
             
           </span> */}
         </a>
-     
+
         <div className="flex md:order-2">
-          <button
-            type="button"
-            data-collapse-toggle="navbar-search"
-            aria-controls="navbar-search"
-            aria-expanded="false"
+          <Button
+            variant={"secondary"}
             className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
           >
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            <span className="sr-only">
-              Search
-            </span>
-          </button>
+            <ShoppingCart className="mr-2 h-4 w-4 font-bold" />
+          </Button>
+
           <div className="relative hidden md:block">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <div className="absolute inset-y-0 left-0 flex items-center px-3 pointer-events-none">
               <svg
                 className="w-5 h-5 text-gray-500"
                 aria-hidden="true"
@@ -73,6 +56,14 @@ export default function Header() {
               className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Search..."
             />
+          </div>
+          <div className="md:px-4">
+            <Button
+              variant={"secondary"}
+              className="hidden md:block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
+            >
+              <ShoppingCart className="mr-2 h-4 w-4 font-bold" />
+            </Button>
           </div>
           <button
             data-collapse-toggle="navbar-search"
@@ -136,11 +127,6 @@ export default function Header() {
               >
                 All Products
               </a>
-            </li>
-            <li>
-              <Button variant={"ghost"}>
-                <ShoppingCart className="mr-2 h-4 w-4 font-bold" />
-              </Button>
             </li>
             {/* <li>
               <a
