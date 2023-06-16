44da9110937e6d5250ebf9aa35d8d3a691661297
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-06-16 13:37:15 +0500

    FIX: Header alignment updated.

--- components/header.tsx
+++ components/header.tsx
@@ -74,7 +74,7 @@ export default function Header() {
             </Button> */}
           </div>
 
-          <div className="relative hidden md:block">
+          <div className="relative py-2 hidden md:block">
             <div className="absolute inset-y-0 left-0 flex items-center px-3 pointer-events-none">
               <svg
                 className="w-5 h-5 text-gray-500"
