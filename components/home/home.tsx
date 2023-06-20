6b25913f3f8d48895e224eaa4dcd6272195253a3
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-06-20 15:45:51 +0500

    FEAT: Designing home page header.

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -3,8 +3,12 @@ import React from 'react'
 export default function HomeComponent() {
   return (
     <div className='container'>
-        <div className="flex">
-            <h1>This is Home Page</h1>
+        <div className="flex justify-around">
+            <div className="flex">
+              <div className="h-10 w-32 bg-blue-100 rounded-sm flex justify-center items-center">
+                <p className="text-blue-600 font-bold">Sale 70%</p>
+              </div>
+            </div>
         </div>
     </div>
   )
