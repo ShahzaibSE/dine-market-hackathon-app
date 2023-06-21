b427b4a195ec85db1057aad1bebe21000e99b855
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-06-21 22:59:30 +0500

    FEAT: Creatiung event cards for index page.

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -39,13 +39,21 @@ export default function HomeComponent() {
         <div>
           <div className="hidden md:flex md:relative md:bottom-14 md:justify-start md:items-start md:w-[600px] md:h-[600px] md:rounded-full md:bg-orange-200">
             <Image
-              className="md:relative left-14"
+              className="md:relative left-14 pt-5"
               src="/assets/homepage-picture.webp"
               alt="dine market front image"
               width="700"
               height="700"
             />
           </div>
+        </div>
+      </div>
+      <div className="flex flex-col mt-10">
+        <div className="flex justify-center items-start">
+          <p className="font-bold text-blue-600 text-base">PROMOTIONS</p>
+        </div>
+        <div className="flex justify-center items-start">
+          <h1 className="text-3xl font-bold tracking-wider md:text-4xl">Our Promotions Events</h1>
         </div>
       </div>
     </div>
