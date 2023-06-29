059f81e0dfa6bf31a0036565627aef0007a8c398
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-06-29 19:02:45 +0500

    FEAT: Added some animation for featured products.

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useRef, useState } from "react";
+import { motion } from "framer-motion";
 import {
   Swiper,
   SwiperSlide,
@@ -233,79 +234,85 @@ export default function HomeComponent() {
 
       <div
         id="featured-products"
-        className="flex flex-col md:flex-row justify-between items-center my-12"
+        className="flex flex-col md:flex-row justify-between items-center my-12 gap-4"
       >
-        <Card>
-          <CardContent>
-            <div className="flex justify-center items-center">
-              <Image
-                alt="female-brushed-reglan-sweatshirt"
-                src="/assets/female-clothes/female-brushed-raglan-sweatshirt.png"
-                width="300"
-                height="320"
-                className="max-w-full"
-              />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <div className="flex flex-col justify-between items-center gap-2">
-              <h3 className="font-bold text-2xl md:3xl">
-                Brushed Reglan Sweatshirt
-              </h3>
-              <h3 className="font-bold text-2xl md:3xl">
-                $195
-              </h3>
-            </div>
-          </CardFooter>
-        </Card>
+        <motion.div whileHover={{ scale: 1.2 }}>
+          <Card>
+            <CardContent>
+              <div className="flex justify-center items-center">
+                <Image
+                  alt="female-brushed-reglan-sweatshirt"
+                  src="/assets/female-clothes/female-brushed-raglan-sweatshirt.png"
+                  width="300"
+                  height="320"
+                  className="max-w-full"
+                />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <div className="flex flex-col justify-between items-center gap-2">
+                <h3 className="font-bold text-2xl md:3xl">
+                  Brushed Reglan Sweatshirt
+                </h3>
+                <h3 className="font-bold text-2xl md:3xl">
+                  $195
+                </h3>
+              </div>
+            </CardFooter>
+          </Card>
+        </motion.div>
 
-        <Card>
-          <CardContent>
-            <div className="flex justify-center items-center">
-              <Image
-                alt="female-brushed-reglan-sweatshirt"
-                src="/assets/female-clothes/cameryn-sash-tie-dress-1.png"
-                width="300"
-                height="320"
-                className="max-w-full"
-              />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <div className="flex flex-col justify-between items-center gap-2">
-              <h3 className="font-bold text-2xl md:3xl">
-                Cameryn Sash Tie Dress
-              </h3>
-              <h3 className="font-bold text-2xl md:3xl">
-                $545
-              </h3>
-            </div>
-          </CardFooter>
-        </Card>
+        <motion.div whileHover={{ scale: 1.2 }}>
+          <Card>
+            <CardContent>
+              <div className="flex justify-center items-center">
+                <Image
+                  alt="female-brushed-reglan-sweatshirt"
+                  src="/assets/female-clothes/cameryn-sash-tie-dress-1.png"
+                  width="300"
+                  height="320"
+                  className="max-w-full"
+                />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <div className="flex flex-col justify-between items-center gap-2">
+                <h3 className="font-bold text-2xl md:3xl">
+                  Cameryn Sash Tie Dress
+                </h3>
+                <h3 className="font-bold text-2xl md:3xl">
+                  $545
+                </h3>
+              </div>
+            </CardFooter>
+          </Card>
+        </motion.div>
 
-        <Card>
-          <CardContent>
-            <div className="flex justify-center items-center">
-              <Image
-                alt="female-brushed-reglan-sweatshirt"
-                src="/assets/female-clothes/female-flex-sweatshirt.png"
-                width="300"
-                height="320"
-                className="max-w-full"
-              />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <div className="flex flex-col justify-between items-center gap-2">
-              <h3 className="font-bold text-2xl md:3xl">
-                Flex Sweatshirt
-              </h3>
-              <h3 className="font-bold text-2xl md:3xl">
-                $175
-              </h3>
-            </div>
-          </CardFooter>
-        </Card>
+        <motion.div whileHover={{ scale: 1.2 }}>
+          <Card>
+            <CardContent>
+              <div className="flex justify-center items-center">
+                <Image
+                  alt="female-brushed-reglan-sweatshirt"
+                  src="/assets/female-clothes/female-flex-sweatshirt.png"
+                  width="300"
+                  height="320"
+                  className="max-w-full"
+                />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <div className="flex flex-col justify-between items-center gap-2">
+                <h3 className="font-bold text-2xl text-center md:3xl">
+                  Brushed Flex Sweatshirt
+                </h3>
+                <h3 className="font-bold text-2xl md:3xl">
+                  $175
+                </h3>
+              </div>
+            </CardFooter>
+          </Card>
+        </motion.div>
       </div>
       {/* <div>
         <HomeSlider />
