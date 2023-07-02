a79b76589c46f11b83043c1f6cf8d7eda7f2dcea
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-07-02 15:26:38 +0500

    FEAT: Making a responsive fotter.

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import {
+  Facebook,
+  Twitter,
+  Linkedin,
+} from "lucide-react";
+import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div>footer</div>
-  )
+    <div className="mx-auto w-full max-w-screen-xl flex flex-wrap relative bottom-0 p-12 md:p-16">
+      <div className="flex flex-row justify-between items-start">
+        <div className="flex flex-col justify-start items-start gap-8">
+          <div>
+            <Image
+              className="aspect-auto"
+              alt="Dine Market logo"
+              src="/assets/dine-market-logo.png"
+              width={180}
+              height={30}
+            />
+          </div>
+          <div className="w-full md:w-2/4">
+            <p className="leading-2 text-lg text-gray-500">Small, artisan label that offers a thoughtfully curated collection of high quality everyday essentials made.</p>
+          </div>
+          <div className="flex flex-row justify-between items-center gap-4">
+            <div>
+              <Link href="#">
+                <Twitter />
+              </Link>
+            </div>
+            <div>
+              <Link href="#">
+                <Facebook />
+              </Link>
+            </div>
+            <div>
+              <Link href="#">
+                <Linkedin />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import HomeComponent from "../../components/home/home";
 import Header from "../../components/header";
+import Footer from "../../components/footer";
 
 export default function Home() {
   return (
@@ -8,9 +9,12 @@ export default function Home() {
       <header>
         <Header />
       </header>
-      <main className="flex flex-wrap max-h-screen p-12 md:p-24">
+      <main className="flex flex-wrap p-12 md:p-24">
         <HomeComponent />
       </main>
+      <footer>
+        <Footer/>
+      </footer>
     </div>
   );
 }
