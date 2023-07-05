1f31747ce7ad1de7bfd0112983e5d040e637c50b
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-07-05 22:11:46 +0500

    FIX: Corrected token for sanity content access

--- src/app/female/page.tsx
+++ src/app/female/page.tsx
@@ -1,22 +1,35 @@
-import React from 'react';
-import FemaleComponent from '../../../components/female_product/female';
-import Header from '../../../components/header';
-import Footer from '../../../components/footer';
+import React from "react";
+import FemaleComponent from "../../../components/female_product/female";
+import Header from "../../../components/header";
+import Footer from "../../../components/footer";
+import { client } from "../../../sanity/lib/client";
+
+export const getFemaleProducts =
+  async function () {
+    try {
+      const res = await client.fetch(
+        `*[_type == 'product' && gender == 'female'] `
+      );
+      return res;
+    } catch (err) {
+      throw err;
+    }
+  };
 
 export default async function FemalePage() {
-  const femaleProducts = await fetch("http:localhost:3000/product");
+  const femaleProducts = await getFemaleProducts();
   console.log(femaleProducts);
   return (
     <div>
-    <header>
-      <Header />
-    </header>
-    <main className="flex flex-wrap p-12 md:p-24">
-      <FemaleComponent />
-    </main>
-    <footer>
-      <Footer/>
-    </footer>
-  </div>
-  )
+      <header>
+        <Header />
+      </header>
+      <main className="flex flex-wrap p-12 md:p-24">
+        <FemaleComponent />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 }
