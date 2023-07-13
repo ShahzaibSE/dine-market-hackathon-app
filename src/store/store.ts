ea0a8072f651d6b3a312c68485ae32804833256d
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-07-13 17:06:49 +0500

    FEAT: Added cart reducer in the store reducers.

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchReducer from "./productSlice";
 import {productAPI} from "./productAPI";
+import { cartReducer } from "./cartSlice";
 
 
 export const store = configureStore({
     reducer: {
         search: searchReducer,
-        productAPI: productAPI.reducer
+        productAPI: productAPI.reducer,
+        cart: cartReducer
     },
     middleware(getDefaultMiddleware) {
         return getDefaultMiddleware().concat(productAPI.middleware);
