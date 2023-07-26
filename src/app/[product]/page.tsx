3b17077e9c94c7bd3fbd0eec6849e6fb04e669b2
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-07-27 00:08:55 +0500

    FIX: Throwing exception.

--- src/app/[product]/page.tsx
+++ src/app/[product]/page.tsx
@@ -306,6 +306,7 @@ export default async function GiveProduct({
     );
   } catch (err) {
     console.log(err);
+    throw err;
     return (
       <div>Product Details Not Available</div>
     );
