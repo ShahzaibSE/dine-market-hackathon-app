e11228b7b1a9d7f70a71b2a5019c261377dd7948
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-07-26 22:06:41 +0500

    FIX: Trying to fix null values.

--- src/app/[product]/page.tsx
+++ src/app/[product]/page.tsx
@@ -51,6 +51,19 @@ async function getProduct(name: string) {
 //   return { props: { repo } }
 // }
 
+const getProductDetails = async function (
+  product_id: string
+) {
+  try {
+    const res = await client.fetch(
+      `*[_type == 'product' && _id == '${product_id}'] `
+    );
+    return res;
+  } catch (err) {
+    throw err;
+  }
+};
+
 export default async function GiveProduct({
   params,
   searchParams,
@@ -58,22 +71,27 @@ export default async function GiveProduct({
   params: { product: string };
   searchParams: { id: string };
 }) {
-  try{
-  const { data } = await (
-    await fetch(
-      `http:localhost:3000/api/productDetails?q=${params.product}`
-    )
-  ).json();
-  // const [currentImage, setcurrentImage] = useState();
-  const product_details: Product = data[0];
-  return (
-    <div className="max-w-screen-xl flex flex-col justify-start items-center gap-20">
-      <div className="xl:container flex flex-wrap xl:flex-row gap-10">
-        {/* <div className="flex">
+  try {
+    const product_detailss =
+      await getProductDetails(params.product);
+    const { data } = await (
+      await fetch(
+        `http:localhost:3000/api/productDetails?q=${params.product}`
+      )
+    ).json();
+    // const [currentImage, setcurrentImage] = useState();
+    // const product_details: Product = data[0];
+    const product_details: Product = product_detailss[0];
+    console.log("Getting Product Details");
+    console.log(product_detailss);
+    return (
+      <div className="max-w-screen-xl flex flex-col justify-start items-center gap-20">
+        <div className="xl:container flex flex-wrap xl:flex-row gap-10">
+          {/* <div className="flex">
         My name is{" "}
         {data[0].name}
       </div> */}
-        {/* <div className="flex flex-row gap-4">
+          {/* <div className="flex flex-row gap-4">
           <div className="flex">
             <div className="flex flex-col justify-start items-start gap-4">
               <div className="flex flex-col justify-between items-center gap-4">
@@ -122,169 +140,173 @@ export default async function GiveProduct({
             </div>
           </div>
         </div> */}
-        <ProductPreview product={product_details} />
-        <div className="flex flex-col gap-6">
-          <div className="flex flex-col">
-            <h2 className="tracking-widest text-2xl">
-              {product_details?.name}
-            </h2>
-            <h3 className="text-gray-400 font-bold text-xl tracking-wide">
-              Jacket
-            </h3>
-          </div>
-
-          <div className="flex flex-col">
-            <div>
-              <p className="text-md font-bold tracking-normal">
-                SELECT SIZE
-              </p>
-            </div>
-            <div className="flex flex-row justify-between items-center gap-4">
-              <div className="w-10 h-10 flex justify-center items-center hover:rounded-full hover:bg-slate-50 hover:border-gray-300 hover:border-4">
-                <h3 className="font-bold text-md">
-                  XS
-                </h3>
-              </div>
-              <div className="w-10 h-10 flex justify-center items-center  hover:rounded-full hover:bg-slate-50 hover:border-gray-300 hover:border-4">
-                <h3 className="font-bold text-md">
-                  S
-                </h3>
-              </div>
-              <div className="w-10 h-10 flex justify-center items-center  hover:rounded-full hover:bg-slate-50 hover:border-gray-300 hover:border-4">
-                <h3 className="font-bold text-md">
-                  M
-                </h3>
-              </div>
-              <div className="w-10 h-10 flex justify-center items-center  hover:rounded-full hover:bg-slate-50 hover:border-gray-300 hover:border-4">
-                <h3 className="font-bold text-md">
-                  L
-                </h3>
-              </div>
-              <div className="w-10 h-10 flex justify-center items-center  hover:rounded-full hover:bg-slate-50 hover:border-gray-300 hover:border-4">
-                <h3 className="font-bold text-md">
-                  XL
-                </h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="flex flex-row justify-start items-center gap-6">
-            <div className="flex justify-center items-center">
-              <p className="font-bold text-md">
-                Quantity:
-              </p>
-            </div>
-            <div className="flex justify-start items-center gap-4">
-              <div className="flex justify-center items-center w-10 h-10 rounded-full bg-gray-400">
-                <span>
-                  <Minus />
-                </span>
-              </div>
-              <div>
-                <p>1</p>
-              </div>
-              <div className="flex justify-center items-center w-10 h-10 rounded-full border-black border-2">
-                <span>
-                  <Plus />
-                </span>
-              </div>
-            </div>
-          </div>
-
-          <div className="flex gap-4">
-            <div className="flex">
-              <AddToCart
-                product={product_details}
-              />
-            </div>
-            <div className="flex justify-center items-center">
-              <h3 className="font-bold text-xl">
-                $225.00
+          <ProductPreview
+            product={product_details}
+          />
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col">
+              <h2 className="tracking-widest text-2xl">
+                {product_details?.name}
+              </h2>
+              <h3 className="text-gray-400 font-bold text-xl tracking-wide">
+                Jacket
               </h3>
             </div>
-          </div>
-        </div>
-        {/* <ProductDetail product_detail={currentProduct} /> */}
-      </div>
 
-      <div className="xl:container flex flex-row justify-start items-start gap-6">
-        <div className="flex flex-col justify-start items-start gap-6">
-          <div className="z-20 absolute">
-            <h1 className="font-bold text-6xl xl:text-9xl opacity-25 text-gray-300 tracking-normal xl:tracking-wider leading-loose xl:leading-none">
-              Overview
-            </h1>
+            <div className="flex flex-col">
+              <div>
+                <p className="text-md font-bold tracking-normal">
+                  SELECT SIZE
+                </p>
+              </div>
+              <div className="flex flex-row justify-between items-center gap-4">
+                <div className="w-10 h-10 flex justify-center items-center hover:rounded-full hover:bg-slate-50 hover:border-gray-300 hover:border-4">
+                  <h3 className="font-bold text-md">
+                    XS
+                  </h3>
+                </div>
+                <div className="w-10 h-10 flex justify-center items-center  hover:rounded-full hover:bg-slate-50 hover:border-gray-300 hover:border-4">
+                  <h3 className="font-bold text-md">
+                    S
+                  </h3>
+                </div>
+                <div className="w-10 h-10 flex justify-center items-center  hover:rounded-full hover:bg-slate-50 hover:border-gray-300 hover:border-4">
+                  <h3 className="font-bold text-md">
+                    M
+                  </h3>
+                </div>
+                <div className="w-10 h-10 flex justify-center items-center  hover:rounded-full hover:bg-slate-50 hover:border-gray-300 hover:border-4">
+                  <h3 className="font-bold text-md">
+                    L
+                  </h3>
+                </div>
+                <div className="w-10 h-10 flex justify-center items-center  hover:rounded-full hover:bg-slate-50 hover:border-gray-300 hover:border-4">
+                  <h3 className="font-bold text-md">
+                    XL
+                  </h3>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-row justify-start items-center gap-6">
+              <div className="flex justify-center items-center">
+                <p className="font-bold text-md">
+                  Quantity:
+                </p>
+              </div>
+              <div className="flex justify-start items-center gap-4">
+                <div className="flex justify-center items-center w-10 h-10 rounded-full bg-gray-400">
+                  <span>
+                    <Minus />
+                  </span>
+                </div>
+                <div>
+                  <p>1</p>
+                </div>
+                <div className="flex justify-center items-center w-10 h-10 rounded-full border-black border-2">
+                  <span>
+                    <Plus />
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex gap-4">
+              <div className="flex">
+                <AddToCart
+                  product={product_details}
+                />
+              </div>
+              <div className="flex justify-center items-center">
+                <h3 className="font-bold text-xl">
+                  $225.00
+                </h3>
+              </div>
+            </div>
           </div>
-          <div className="flex w-2/4 xl:w-full justify-start items-start relative top-10 xl:top-16">
-            <h2 className="text-2xl xl:text-3xl font-bold tracking-wide leading-8">
-              Product Information
-            </h2>
+          {/* <ProductDetail product_detail={currentProduct} /> */}
+        </div>
+
+        <div className="xl:container flex flex-row justify-start items-start gap-6">
+          <div className="flex flex-col justify-start items-start gap-6">
+            <div className="z-20 absolute">
+              <h1 className="font-bold text-6xl xl:text-9xl opacity-25 text-gray-300 tracking-normal xl:tracking-wider leading-loose xl:leading-none">
+                Overview
+              </h1>
+            </div>
+            <div className="flex w-2/4 xl:w-full justify-start items-start relative top-10 xl:top-16">
+              <h2 className="text-2xl xl:text-3xl font-bold tracking-wide leading-8">
+                Product Information
+              </h2>
+            </div>
+          </div>
+        </div>
+
+        <div className="hidden xl:block"></div>
+
+        <div className="flex flex-col justify-between items-center gap-6 xl:container">
+          <div className="flex flex-col xl:flex-row xl:container justify-between items-start gap-2">
+            <div className="flex flex-col justify-start items-start">
+              <h3 className="font-bold text-lg xl:text-xl text-gray-400 tracking-wider">
+                PRODUCT DETAILS
+              </h3>
+            </div>
+            <div className="flex justify-start items-start xl:w-4/6">
+              <p className="text-lg leading-8 tracking-wider text-gray-600 text-justify">
+                Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit, sed
+                do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud
+                exercitation ullamco laboris nisi
+                ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor
+                in reprehenderit in voluptate
+                velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint
+                occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt
+                mollit anim id est laborum.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col xl:flex-row justify-between items-start xl:container gap-4 xl:gap-0 mt-6 xl:mt-0">
+            <div className="flex flex-row justify-start items-start">
+              <h3 className="font-bold text-lg xl:text-xl text-gray-400 tracking-wider">
+                PRODUCT CARE
+              </h3>
+            </div>
+            <div className="flex justify-start items-start xl:w-4/6">
+              <ul className="list-disc">
+                <li>
+                  <p className="font-bold text-lg leading-8 tracking-wide">
+                    Hand wash using cold water.
+                  </p>
+                </li>
+                <li>
+                  <p className="font-bold text-lg leading-8 tracking-wide">
+                    Do not using bleach.
+                  </p>
+                </li>
+                <li>
+                  <p className="font-bold text-lg leading-8 tracking-wide">
+                    Hang it to dry.
+                  </p>
+                </li>
+                <li>
+                  <p className="font-bold text-lg leading-8 tracking-wide">
+                    Iron on low temperature.
+                  </p>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-
-      <div className="hidden xl:block"></div>
-
-      <div className="flex flex-col justify-between items-center gap-6 xl:container">
-        <div className="flex flex-col xl:flex-row xl:container justify-between items-start gap-2">
-          <div className="flex flex-col justify-start items-start">
-            <h3 className="font-bold text-lg xl:text-xl text-gray-400 tracking-wider">
-              PRODUCT DETAILS
-            </h3>
-          </div>
-          <div className="flex justify-start items-start xl:w-4/6">
-            <p className="text-lg leading-8 tracking-wider text-gray-600 text-justify">
-              Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore
-              et dolore magna aliqua. Ut enim ad
-              minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat.
-              Duis aute irure dolor in
-              reprehenderit in voluptate velit
-              esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat
-              cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit
-              anim id est laborum.
-            </p>
-          </div>
-        </div>
-        <div className="flex flex-col xl:flex-row justify-between items-start xl:container gap-4 xl:gap-0 mt-6 xl:mt-0">
-          <div className="flex flex-row justify-start items-start">
-            <h3 className="font-bold text-lg xl:text-xl text-gray-400 tracking-wider">
-              PRODUCT CARE
-            </h3>
-          </div>
-          <div className="flex justify-start items-start xl:w-4/6">
-            <ul className="list-disc">
-              <li>
-                <p className="font-bold text-lg leading-8 tracking-wide">
-                  Hand wash using cold water.
-                </p>
-              </li>
-              <li>
-                <p className="font-bold text-lg leading-8 tracking-wide">
-                  Do not using bleach.
-                </p>
-              </li>
-              <li>
-                <p className="font-bold text-lg leading-8 tracking-wide">
-                  Hang it to dry.
-                </p>
-              </li>
-              <li>
-                <p className="font-bold text-lg leading-8 tracking-wide">
-                  Iron on low temperature.
-                </p>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-      }catch(err){
-        return (<div>Product Details Not Available</div>)
-      }
+    );
+  } catch (err) {
+    return (
+      <div>Product Details Not Available</div>
+    );
+  }
 }
