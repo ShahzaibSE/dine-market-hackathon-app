98ec33441ba3008d7ef8ca44e4247b1853448be5
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-08-12 00:13:38 +0500

    FEAT: Created endpoints for add to cart functionality and integerated vercel/postgresql database

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -1,16 +1,74 @@
 import { Product } from "@/type";
-import { NextRequest, NextResponse } from "next/server";
-import { dbClient } from "@/lib/drizzle";
+import {
+  NextRequest,
+  NextResponse,
+} from "next/server";
+import {
+  dbClient,
+  cartTable,
+} from "@/lib/drizzle";
 
-export async function POST(request: NextRequest, response: NextResponse) {
-    try{
+import {v4 as uuid} from "uuid";
+import { cookies } from "next/headers";
 
-    }catch(err){
-        return NextResponse.json({
-            status: false,
-            resCode: 500,
-            message: "Internal Error",
-            isError: true
-        })
+export const GET = async function () {
+  try {
+    const cartItems = await dbClient
+      .select()
+      .from(cartTable);
+
+    //
+    return NextResponse.json({
+      status: true,
+      resCode: 200,
+      message: "Your Products in Cart",
+      data: cartItems,
+      isError: false,
+    });
+  } catch (err) {
+    return NextResponse.json({
+      status: false,
+      resCode: 500,
+      message: "Something Went Wrong",
+      isError: true,
+    });
+  }
+};
+
+export const POST = async function (
+  request: NextRequest,
+  response: NextResponse
+) {
+  try {
+    const req = await request.json();
+    const uid = uuid();
+    const setCookies = cookies();
+    //
+    if(!cookies().has("user_id")){
+      setCookies.set("user_id", uid);
     }
-}
+    //
+    const data = await dbClient
+      .insert(cartTable)
+      .values({
+        product_id: req.product_id,
+        quantity: 1,
+        user_id: setCookies.get("user_id")?.value as string
+      });
+    //
+    return NextResponse.json({
+      status: true,
+      resCode: 200,
+      message: "Product added to cart",
+      data,
+      isError: false,
+    });
+  } catch (err) {
+    return NextResponse.json({
+      status: false,
+      resCode: 500,
+      message: "Internal Error",
+      isError: true,
+    });
+  }
+};
--- src/lib/drizzle.ts
+++ src/lib/drizzle.ts
@@ -5,7 +5,7 @@ import {
   integer,
 } from "drizzle-orm/pg-core";
 import { sql } from "@vercel/postgres";
-import {drizzle} from "drizzle-orm/vercel-postgres";
+import { drizzle } from "drizzle-orm/vercel-postgres";
 
 export const cartTable = pgTable("cart", {
   id: serial("id").primaryKey(),
@@ -13,6 +13,9 @@ export const cartTable = pgTable("cart", {
     length: 255,
   }).notNull(),
   quantity: integer("quantity").notNull(),
+  user_id: varchar("product_id", {
+    length: 255,
+  }).notNull(),
 });
 
 export const dbClient = drizzle(sql);
